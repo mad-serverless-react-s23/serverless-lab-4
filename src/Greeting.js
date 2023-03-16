d0d7c6e159bbd94133fefa6d4270d5a22c73db64
MysticalSkeptic <115025255+MysticalSkeptic@users.noreply.github.com>
2023-03-16 14:41:19 -0500

    first functional draft
    now to get the list items fixed...

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,3 +1,33 @@
-export const Greeting = () => {
-    
-}
+const people = [
+    {
+      name: 'Donna',
+      favoriteNumber: 22,
+      favoriteColor: ['Blue', 'White', 'Orange']
+    },
+    {
+      name: 'Jake',
+      favoriteNumber: 6,
+      favoriteColor: ['Yellow']
+    },
+    {
+      name: 'Alberto',
+      favoriteNumber: 99,
+      favoriteColor: ['Aquamarine', 'Fucsia']
+    }
+  ];
+export const Greeting = () => (
+    <>
+        {people.map(x => (
+            <div>
+                <h3>Hello, {x.name}</h3>
+                <p>{x.name}'s favorite number is {x.favoriteNumber}</p>
+                <p>{x.name}'s favorite number plus 10 is {x.favoriteNumber + 10}</p>
+                <p>{x.name}'s favorite color is 
+                    <ul>
+                        {x.favoriteColor.map((y, i) => y.i === 0 ? <li>None</li> : <li>{y.i}</li>)}
+                    </ul>
+                </p>
+            </div>
+        ))}
+    </>
+)
